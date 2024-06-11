e05729826bc1433b174f36c986cc244041e5c86c
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-06-11 18:30:52 +0530

    initial commit

--- JS/6_11/1.js
+++ JS/6_11/1.js
@@ -12,5 +12,21 @@ var x = {name:"Ritik",age:23}; //Object
 //when adding a number and string,js will treat number as a string
 var data = 3 + "3";
 console.log(data);
+var data2 = "yup" + 3 + 2;
+console.log(data2);
+var data3 = 12+6+"yup";
+console.log(data3);
+
+var xy=1;
+var x2 = 123e5;
+console.log(xy,x2);
+
+var x5=true;
+var x6=false;
+console.log(x5,x6);
+
+var p1= {name:"pratik",age:23,designation:"Frontend"}
+console.log(p1['name']);
+
 
 
