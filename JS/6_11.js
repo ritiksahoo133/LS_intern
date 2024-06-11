59efa0786547c6d57fd1436d3ae0502ffa56f1ec
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-06-11 15:40:53 +0530

    introduction of js completed

---
+++ JS/6_11.js
@@ -0,0 +1,5 @@
+// javascript has window object
+// Light-weight programming Language
+//cross platform = runs on multiple platform
+//client side scripting Language
+//text/javascript=provides information to the browser about data
