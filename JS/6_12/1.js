fbf2fabbb031ba205fd1d68262aa6c92eb2e56ae
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-06-12 16:02:39 +0530

    last index

--- JS/6_12/1.js
+++ JS/6_12/1.js
@@ -1,16 +1,20 @@
-let car ={
-      model:'sonet',
-      cname:'kia',
-      saftey:2
+let car = {
+      model: 'sonet',
+      cname: 'kia',
+      saftey: 2
 }
 console.log(car);
 let key = "model"
 console.log(car[key]);
 
-fullName = "RitikSahoo";
+fullName = "RitikSahioo";
 console.log(fullName.toUpperCase());
 console.log(fullName.toLowerCase());
-console.log(fullName.slice(1,fullName.length-2));
+console.log(fullName.slice(1, fullName.length - 2));
 console.log(fullName.slice(2));
+console.log(fullName.charAt(2));
+console.log(fullName.indexOf('i', 2));
+console.log(fullName.lastIndexOf('i'));
+
 
 
