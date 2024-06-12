95234845ed729b1da19b38ccd0ab39447a10c5c8
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-06-12 17:48:00 +0530

    string method

--- JS/6_12/1.js
+++ JS/6_12/1.js
@@ -7,14 +7,26 @@ console.log(car);
 let key = "model"
 console.log(car[key]);
 
-fullName = "RitikSahioo";
-console.log(fullName.toUpperCase());
-console.log(fullName.toLowerCase());
-console.log(fullName.slice(1, fullName.length - 2));
-console.log(fullName.slice(2));
+fullName = "RitikiSahoo";
+// console.log(fullName.toUpperCase());
+// console.log(fullName.toLowerCase());
+// console.log(fullName.slice(1, fullName.length - 2));
+// console.log(fullName.slice(2));
 console.log(fullName.charAt(2));
 console.log(fullName.indexOf('i', 2));
-console.log(fullName.lastIndexOf('i'));
+// console.log(fullName.lastIndexOf('i'));
+// console.log(fullName.trim());
+// console.log(fullName.trimStart());
+// console.log(fullName.search('Sah'));
+let xx = new String('Hello');
+let xy = new String('Hello');
+
+console.log(typeof xx, typeof xy)
+console.log(xy == xx);
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// console.log(fruits.slice(-3, -1));
+
+
 
 
 
