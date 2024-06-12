cc0d3a86d883f4c3146b8c8653888a6cb416d1f8
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-06-12 15:50:57 +0530

    slice

--- JS/6_12/1.js
+++ JS/6_12/1.js
@@ -10,4 +10,7 @@ console.log(car[key]);
 fullName = "RitikSahoo";
 console.log(fullName.toUpperCase());
 console.log(fullName.toLowerCase());
+console.log(fullName.slice(1,fullName.length-2));
+console.log(fullName.slice(2));
+
 
