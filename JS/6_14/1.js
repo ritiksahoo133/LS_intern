1095130eac71ced05adc67ffae65316830f6b432
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-06-14 11:45:58 +0530

    parseFloat method

--- JS/6_14/1.js
+++ JS/6_14/1.js
@@ -12,3 +12,11 @@ console.log(parseInt("123"));
 console.log(parseInt("123hello"));
 console.log(parseInt("123.45")); //decimal point and everything after it is ignored
 console.log(parseInt("hello")); //NaN
+
+//------parseFloat------
+// console.log(parseFloat("123"));       
+// console.log(parseFloat("123.45"));   
+// console.log(parseFloat("3.14"));      
+// console.log(parseFloat("10.5 meters"));  
+// console.log(parseFloat("hello"));
+
