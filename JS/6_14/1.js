b868eac8574e765a978364f9ddbb4fc6771d15d1
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-06-14 11:24:57 +0530

    Number() method

---
+++ JS/6_14/1.js
@@ -0,0 +1,7 @@
+console.log(Number("123"));  //123
+console.log(Number(true));  //1
+console.log(Number(false));  //0
+console.log(Number(null)); //0
+console.log(Number(undefined)); //NaN
+console.log(Number("hello"));  //NaN
+console.log(Number({}));  //NaN
