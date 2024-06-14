b4198637b5541e50746d4f4edf378d201b26f711
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-06-14 11:42:55 +0530

    parseInt method

--- JS/6_14/1.js
+++ JS/6_14/1.js
@@ -7,3 +7,8 @@ console.log(Number("hello"));  //NaN
 console.log(Number({}));  //NaN
 console.log(Number("10 "));
 
+// ------parseInt------
+console.log(parseInt("123"));
+console.log(parseInt("123hello"));
+console.log(parseInt("123.45")); //decimal point and everything after it is ignored
+console.log(parseInt("hello")); //NaN
