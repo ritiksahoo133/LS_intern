cbe9ea55225c3610a6d39376130fc088480827e2
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-06-18 12:23:07 +0530

    continue statement

--- JS/6_18/1.js
+++ JS/6_18/1.js
@@ -1,7 +1,14 @@
 // -----Break------
-for(let i=0;i<10;i++){
-      if(i==5){
+for (let i = 0; i < 10; i++) {
+      if (i == 5) {
             console.log(i);
             break
       }
+}
+// ------continue-------
+for (let i = 0; i < 10; i++){
+      if(i==4){
+            continue
+      }
+      // console.log(i);
 }
