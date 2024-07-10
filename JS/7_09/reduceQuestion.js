132246f850f5c4a558d92d310926e2f99e1f905d
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-07-10 16:01:31 +0530

    reduce Question

---
+++ JS/7_09/reduceQuestion.js
@@ -0,0 +1,419 @@
+const arrayData = [
+  {
+    keys: ["2024-05-14"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-05-15"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-05-16"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-05-17"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-05-18"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-05-19"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-05-20"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-05-21"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-05-22"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-05-23"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-05-24"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-05-25"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-05-26"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-05-27"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-05-28"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-05-29"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-05-30"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-05-31"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-01"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-02"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-03"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-04"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-05"],
+    clicks: 0,
+    impressions: 4,
+    ctr: 0,
+    position: 1,
+  },
+  {
+    keys: ["2024-06-06"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-07"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-08"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-09"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-10"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-11"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-12"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-13"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-14"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-15"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-16"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-17"],
+    clicks: 2,
+    impressions: 3,
+    ctr: 0.6666666666666666,
+    position: 1,
+  },
+  {
+    keys: ["2024-06-18"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-19"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-20"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-21"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-22"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-23"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-24"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-25"],
+    clicks: 0,
+    impressions: 3,
+    ctr: 0,
+    position: 1,
+  },
+  {
+    keys: ["2024-06-26"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-27"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-28"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-29"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-06-30"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-07-01"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-07-02"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-07-03"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-07-04"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-07-05"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+  {
+    keys: ["2024-07-06"],
+    clicks: 0,
+    impressions: 0,
+    ctr: 0,
+    position: 0,
+  },
+];
+// let keysArr = [];
+// const result = arrayData.reduce(
+//   (acc, curr) => {
+//     acc.keys.push(...curr.keys);
+//     acc.clicks.push(curr.clicks);
+//     acc.impressions.push(curr.impressions);
+//     acc.ctr.push(curr.ctr);
+//     acc.position.push(curr.position);
+//     return acc;
+//   },
+//   {
+//     keys: [],
+//     clicks: [],
+//     impressions: [],
+//     ctr: [],
+//     position: [],
+//   }
+// );
+// console.log(result);
+
+let keys = [];
+let clicks = [];
+let impressions = [];
+let ctr = [];
+let position = [];
+const result = arrayData.reduce((acc, curr) => {
+  keys.push(curr.keys[0]);
+  clicks.push(curr.clicks);
+  impressions.push(curr.impressions);
+  ctr.push(curr.ctr);
+  position.push(curr.position);
+});
+console.log({
+  keys: keys,
+  clicks: clicks,
+  impressions: impressions,
+  ctr: ctr,
+  position: position,
+});
