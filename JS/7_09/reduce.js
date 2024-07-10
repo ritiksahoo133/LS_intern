2236c0eb46101dcf620288a46af465597458bbb4
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-07-10 16:09:21 +0530

    How many people voted

--- JS/7_09/reduce.js
+++ JS/7_09/reduce.js
@@ -12,4 +12,26 @@ const objects = [{ x: 1 }, { x: 2 }, { x: 3 }];
 const sum = objects.reduce((acc, curr) => {
   return (acc += curr.x);
 }, 0);
-console.log(sum);
+// console.log(sum);
+
+var voters = [
+  { name: "Bob", age: 30, voted: true },
+  { name: "Jake", age: 32, voted: true },
+  { name: "Kate", age: 25, voted: false },
+  { name: "Sam", age: 20, voted: false },
+  { name: "Phil", age: 21, voted: true },
+  { name: "Ed", age: 55, voted: true },
+  { name: "Tami", age: 54, voted: true },
+  { name: "Mary", age: 31, voted: false },
+  { name: "Becky", age: 43, voted: false },
+  { name: "Joey", age: 41, voted: true },
+  { name: "Jeff", age: 30, voted: true },
+  { name: "Zack", age: 19, voted: false },
+];
+const TotalVote = voters.reduce((acc, curr) => {
+  if (curr.voted === true) {
+    acc = acc + 1;
+  }
+  return acc;
+}, 0);
+console.log(TotalVote);
