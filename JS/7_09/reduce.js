2af296722ef2e743fc9c04aa6acbcc20d91d63d0
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-07-10 13:07:22 +0530

    sum of values in an Object array

--- JS/7_09/reduce.js
+++ JS/7_09/reduce.js
@@ -1,7 +1,15 @@
 const arr = [1, 2, 3, 4, 5];
 
 const result = arr.reduce((acc, curr) => {
-  console.log({ acc: acc, curr: curr });
+  //   console.log({ acc: acc, curr: curr });
   return (acc += curr);
 }, 0);
-console.log(result);
+// console.log(result);
+
+// Sum of Values in an Object array
+
+const objects = [{ x: 1 }, { x: 2 }, { x: 3 }];
+const sum = objects.reduce((acc, curr) => {
+  return (acc += curr.x);
+}, 0);
+console.log(sum);
