94510fef9420e5845a85778ff6b5f583be3f2379
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-07-15 10:54:27 +0530

    fetch user data

--- JS/7_12/promises.js
+++ JS/7_12/promises.js
@@ -1,3 +1,5 @@
+var editUser;
+
 async function getData() {
   const response = await fetch("https://reqres.in/api/users?page=2");
   const json = await response.json();
@@ -7,15 +9,38 @@ async function getData() {
     const row = document.createElement("tr");
     row.innerHTML = `
         <td>${user.id}</td>
-        <td><img src="${user.avatar}" alt="Avatar" style="border-radius:100px "></td>
+        <td><img src="${user.avatar}" alt="Avatar" style="border-radius:100px;width:80px;height:80px; margin:2px"></td>
         <td>${user.email}</td>
         <td>${user.first_name}</td>
         <td>${user.last_name}</td>
+        <td><button onclick = "editUserData(${user.id})" id="editButton">Edit</button></td>
+        <td><button id="deleteButton">Delete</button></td>
       `;
     tableBody.appendChild(row);
   });
 }
-
 getData();
-// console.log(user);
-// createFunc(user.data);
+
+async function editUserData(id) {
+  //   console.log(id);
+  const response = await fetch("https://reqres.in/api/users?page=2");
+  const allUserData = await response.json();
+  editUser = allUserData["data"].find((user) => {
+    if (user.id === id) {
+      return user;
+    }
+  });
+  console.log(editUser);
+  window.location.href =
+    window.location.origin + "/LS_intern/JS/7_12/editUserData.html";
+}
+
+function editAfterSubmitFunc() {
+  let email = document.getElementById("eml");
+  let fname = document.getElementById("fname");
+  let lname = document.getElementById("lname");
+
+  editUser.email = email.value;
+  editUser.first_name = fname.value;
+  editUser.lname = lname.value;
+}
