0e9e6e332130c9ae66e250931b15939966a628d4
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-07-15 15:57:29 +0530

    allSettled

--- JS/7_15/allSettled.js
+++ JS/7_15/allSettled.js
@@ -10,3 +10,14 @@ Promise.allSettled(promises).then((results) =>
     console.log(result.status);
   })
 );
+
+Promise.allSettled([
+  Promise.resolve(33),
+  new Promise((resolve) => setTimeout(() => resolve(66)), 0),
+  99,
+  Promise.reject(new Error("an error")),
+]).then((values) => {
+  values.forEach((val) => {
+    console.log(val.value);
+  });
+});
